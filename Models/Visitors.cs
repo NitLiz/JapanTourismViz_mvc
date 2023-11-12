0704f7c1ed97b94dbfc44ac719f03779aaf12fee
(Victoria Helena) Nita Hidayat <helena.nita7@gmail.com>
2023-11-12 18:30:45 +0700

    Initial Commit

---
+++ Models/Visitors.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JapanTourism.Models
+{
+	public class Visitors
+	{
+        public int Id { get; set; }
+        public string? Country { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Month_come { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Year_come { get; set; }
+        //public string? Genre { get; set; }
+        public decimal No_of_visitors { get; set; }
+    }
+}
+
